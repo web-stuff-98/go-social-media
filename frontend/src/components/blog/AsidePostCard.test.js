2d8d18af644619190dd53d6ef0bdea45d98b4f3b
web-stuff-98 <jphoneacc98@gmail.com>
2023-01-25 21:15:29 +0000

    just remembered I deleted setupTests.ts when I started the project, which is why none of my tests worked

---
+++ frontend/src/components/blog/AsidePostCard.test.js
@@ -0,0 +1,33 @@
+import { screen, render } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import AsidePostCard from "./AsidePostCard";
+
+let container = null;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe("aside post card component", () => {
+  test("should render the title of the post inside of a link", () => {
+    render(
+      <AsidePostCard
+        post={{ title: "Post title", slug: "/page/post-title" }}
+      />,
+      container
+    );
+
+    const linkElement = screen.findByRole("link");
+    const titleElement = screen.findByRole("heading");
+
+    expect(linkElement).toBeInTheDocument();
+    expect(titleElement).toBeInTheDocument();
+  });
+});
